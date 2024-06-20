0b26ab51c7775ee7d9da5dbb860fb8f5dde67c22
YazanNassr <yazan.nasser.abdullah@gmail.com>
2024-06-20 15:43:05 +0300

    Display date in Main.java by Mohammad

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Calendar calendar = new Calendar();
+        System.out.println("Today's date is: " + calendar.getTodaysDate());
+    }
+}
